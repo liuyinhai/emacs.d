86d51f15848078505d020f0a22cb85a8b219c7e7
yinhailiu <yinhai.liu@gmail.com>
2014-04-17 15:06:37 +0800

    commit for new files

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 ;;; a number of other files.
 
 (add-to-list 'load-path user-emacs-directory)
-(require 'init-yinhai)
 
 (let ((minver 23))
   (unless (>= emacs-major-version minver)
@@ -137,10 +136,26 @@
             (message "init completed in %.2fms"
                      (sanityinc/time-subtract-millis after-init-time before-init-time))))
 
+(prefer-coding-system 'gbk)
+(setq default-buffer-file-coding-system 'gbk)
+
 
 (provide 'init)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#C7EDCC" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Courier New")))))
 ;; Local Variables:
-;; coding: utf-8
 ;; no-byte-compile: t
 ;; End:
